c28a022cd5cff283348376167829e5dd7b5200a5
mjjonone <142796823+mjjonone@users.noreply.github.com>
2023-12-26 06:29:16 +0000

    update

--- good.sh
+++ good.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 NEZHA_SERVER=${NEZHA_SERVER:-''}
 NEZHA_PORT=${NEZHA_PORT:-''}
@@ -11,6 +11,49 @@ CFIP=${CFIP:-'icook.hk'}
 NAME=${NAME:-''}
 SERVER_PORT="${SERVER_PORT:-${PORT:-3000}}"
 
+check_systemctl() {
+  if ! command -v systemctl &>/dev/null; then
+    CHECK_WGET=$(wget 2>&1 | head -n 1)
+    grep -qi 'busybox' <<< "$CHECK_WGET" && ${PACKAGE_INSTALL[int]} wget >/dev/null 2>&1
+
+    DEPS_CHECK=("bash" "rc-update" "ss" "python3")
+    DEPS_INSTALL=("bash" "openrc" "iproute2" "python3")
+    for ((g=0; g<${#DEPS_CHECK[@]}; g++)); do
+      [ ! $(type -p ${DEPS_CHECK[g]}) ] && [[ ! "${DEPS[@]}" =~ "${DEPS_INSTALL[g]}" ]] && DEPS+=(${DEPS_INSTALL[g]})
+    done
+    if [ "${#DEPS[@]}" -ge 1 ]; then
+      info "\n $(text 7) ${DEPS[@]} \n"
+      ${PACKAGE_UPDATE[int]} >/dev/null 2>&1
+      ${PACKAGE_INSTALL[int]} "${DEPS[@]}" >/dev/null 2>&1
+    else
+      info "\n $(text 8) \n"
+    fi
+
+    [ ! $(type -p systemctl) ] && wget https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py -O /bin/systemctl && chmod a+x /bin/systemctl
+  fi
+}
+
+
+check_systemctl
+
+# Check if .env file exists
+if [ ! -f .env ]; then
+  # If .env file does not exist, initialize it with default values
+  echo "NEZHA_SERVER=$NEZHA_SERVER
+NEZHA_PORT=$NEZHA_PORT
+NEZHA_KEY=$NEZHA_KEY
+TLS=$TLS
+ARGO_DOMAIN=$ARGO_DOMAIN
+WEB_DOMAIN=$WEB_DOMAIN
+ARGO_AUTH=$ARGO_AUTH
+CFIP=$CFIP
+NAME=$NAME
+SERVER_PORT=$SERVER_PORT" > .env
+fi
+
+read -p "Do you want to update the .env file? (y/n) [n]: " update_env
+update_env=${update_env:-"n"}
+
 update_variable() {
   local var_name=$1
   local var_value=$2
@@ -20,34 +63,41 @@ update_variable() {
   change_var=${change_var:-"n"}
   if [ "$change_var" = "y" ]; then
     read -p "Please enter the new value for $var_name: " new_var_value
-    export $var_name=$new_var_value
+    echo "$var_name=$new_var_value" >> .env
   else
-    export $var_name=$var_value
+    echo "$var_name=$var_value" >> .env
   fi
 }
 
-update_variable "ARGO_AUTH" $ARGO_AUTH
-update_variable "NEZHA_SERVER" $NEZHA_SERVER
-update_variable "NEZHA_KEY" $NEZHA_KEY
-update_variable "NEZHA_PORT" $NEZHA_PORT
-update_variable "TLS" $TLS
-update_variable "ARGO_DOMAIN" $ARGO_DOMAIN
-update_variable "WEB_DOMAIN" $WEB_DOMAIN
-update_variable "CFIP" $CFIP
-update_variable "NAME" $NAME
-update_variable "SERVER_PORT" $SERVER_PORT
+if [ "$update_env" = "y" ]; then
+  # Clear the .env file
+  > .env
+  update_variable "ARGO_AUTH" $ARGO_AUTH
+  update_variable "NEZHA_SERVER" $NEZHA_SERVER
+  update_variable "NEZHA_KEY" $NEZHA_KEY
+  update_variable "NEZHA_PORT" $NEZHA_PORT
+  update_variable "TLS" $TLS
+  update_variable "ARGO_DOMAIN" $ARGO_DOMAIN
+  update_variable "WEB_DOMAIN" $WEB_DOMAIN
+  update_variable "CFIP" $CFIP
+  update_variable "NAME" $NAME
+  update_variable "SERVER_PORT" $SERVER_PORT
+fi
 
-echo "ARGO_AUTH: $ARGO_AUTH"
-echo "NEZHA_SERVER: $NEZHA_SERVER"
-echo "NEZHA_KEY: $NEZHA_KEY"
-echo "NEZHA_PORT: $NEZHA_PORT"
-echo "TLS: $TLS"
-echo "ARGO_DOMAIN: $ARGO_DOMAIN"
-echo "WEB_DOMAIN: $WEB_DOMAIN"
-echo "CFIP: $CFIP"
-echo "NAME: $NAME"
-echo "SERVER_PORT: $SERVER_PORT"
-
-curl -SsL https://raw.githubusercontent.com/mjjonone/good/main/vps.sh -o vps.sh
 chmod 755 vps.sh
-nohup bash vps.sh > good.log 2>&1 &
+
+# Create systemd service file
+echo "[Unit]
+Description=GOOD Script
+
+[Service]
+EnvironmentFile=$(pwd)/.env
+ExecStart=/bin/sh -c '$(pwd)/vps.sh > $(pwd)/good.log 2>&1'
+
+[Install]
+WantedBy=multi-user.target" |  tee /etc/systemd/system/good.service
+
+# Reload systemd, enable and start the service
+systemctl daemon-reload
+systemctl enable good.service
+systemctl start good.service
